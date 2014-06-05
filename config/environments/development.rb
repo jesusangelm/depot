7fd4ce3c1250feef85122514801717a0d2a4bc6f
Jesus Marin <jesusangelm@gmail.com>
2014-06-05 12:19:49 -0430

    sending emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Configuracion para envios de correo por servidor SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "testdepot.com.ve",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
+
 end
